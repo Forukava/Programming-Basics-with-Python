15a65ccfd072c8b60becb4d1e80a4f0900f7bbb0
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-09-18 20:24:50 +0300

    Update 04. Clever Lily.py

---
+++ For Loop - Exercise/04. Clever Lily.py
@@ -0,0 +1,25 @@
+print("What is your age: ")
+age = int(input())
+print("How much cost landuary: ")
+price_l = float(input())
+print("price for one toy: ")
+one_toy = int(input())
+
+toy = 0
+cash = 0
+for birthday in range (1, age + 1):
+    if birthday % 2 == 0:
+        cash += birthday * 5
+    else:
+        toy += one_toy
+
+final = toy + cash
+total = final - (age // 2)
+if price_l > total:
+    diff = price_l - total
+    print(f"No! {diff:.2f}")
+else:
+    diff = total - price_l
+    print(f"Yes! {diff:.2f}")
+
+
