a8e5be7cdaa01d509e141f50ff761678fe7dc51e
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-09-19 21:26:46 +0300

    Update 08. Tennis Ranklist.py

--- For Loop - Exercise/08. Tennis Ranklist.py
+++ For Loop - Exercise/08. Tennis Ranklist.py
@@ -1,2 +1,25 @@
-turners = int(input())
+import math
+turnirs = int(input())
 ranglist_number = int(input())
+
+W = 0
+F = 0
+SF = 0
+
+for _ in range(turnirs):
+    stage = input()
+    if stage == "W":
+        W += 2000
+    elif stage == "F":
+        F += 1200
+    elif stage == "SF":
+        SF += 720
+
+total_points = ranglist_number + W + F + SF
+average_points = (total_points - ranglist_number) / turnirs
+won_turnirs = W / 2000
+won_turnirs_percent = won_turnirs / turnirs * 100
+
+print(f"Final points: {total_points}")
+print(f"Average points: {math.floor(average_points)}")
+print(f"{won_turnirs_percent:.2f}%")
