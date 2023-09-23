14b11a99df1d15470c842b4b33068c6d5248b7f6
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-09-23 13:59:45 +0300

    -

---
+++ Tests/9. Auction_program/for loop get value.py
@@ -0,0 +1,5 @@
+#gets the value of dictionary with loop
+bidding_record = {"Angel": 123, "James": 321}
+for bidder in bidding_record:
+    bid_amount =  bidding_record[bidder]
+    print(bid_amount)
