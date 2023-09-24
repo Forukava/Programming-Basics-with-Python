86dc7d594da099467041c5b09b9253afc9661a5d
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-09-24 15:03:50 +0300

    -

--- Tests/Tests 10-19/10.1 Calculator/10.1. Calculator.py
+++ Tests/Tests 10-19/10.1 Calculator/10.1. Calculator.py
@@ -22,11 +22,14 @@ def calculator():
 
     while should_continue:
         operation_symbol = input("Pick an operation: ")
-        num2 = int(input("What's the next number?"))
+        num2 = int(input("What's the next number?:"))
         calculation_function = operations[operation_symbol]
         answer = calculation_function(num1, num2)
+
         print(f"{num1} {operation_symbol} {num2} = {answer}")
         if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
             num1 = answer
         else:
             should_continue = False
+            calculator()
+calculator()
