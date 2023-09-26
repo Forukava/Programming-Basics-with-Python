46ed4c620e3b4f17dcd588b69c36ee4c254aa493
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-09-26 17:26:06 +0300

    Update 02. Exam Preparation.py

--- While Loop - Exercise/02. Exam Preparation.py
+++ While Loop - Exercise/02. Exam Preparation.py
@@ -1,34 +1,24 @@
-bad_score = int(input())
-name_it = input()
-grades = int(input())
-
-count_objects = 0
-count_grades = 0
-
-bad_grades = 0
-last_object = ""
-
-has_failed = True
-
-
-while bad_grades < bad_score:
-    name_it = input()
-    if name_it == "Enough":
-        has_failed = False
-        break
-
-    grades = int(input())
-    if grades < 4:
-        bad_grades += 1
-        count_grades += grades
-        count_objects += 1
-        last_object = name_it
-
-    if has_failed:
-        print(f"You need a break, {bad_score} poor grades.")
-        break
-    else:
-        average = count_grades / count_objects
-        print(f"Average score: {average}")
-        print(f"Number of problem: {count_objects}")
-        print(f"Last problem: {last_object}")
+poorGradeLimit =int(input())
+poorGradeCount = 0
+problemCount = 0
+gradesSum = 0
+lastProblemName = ""
+problemName = ""
+grade = 0
+while True:
+    problemName = input()
+    if problemName != ("Enough"):
+        if grade <= 4:
+            poorGradeCount += 1
+            grade = int(input())
+        if (poorGradeCount >= poorGradeLimit):
+            print(f"You need a break, {poorGradeCount} poor grades.")
+            break
+        gradesSum += grade
+        lastProblemName = problemName
+        problemCount += 1
+        continue
+    print(f"Average score: {(gradesSum / problemCount):.2f}")
+    print(f"Number of problems: {problemCount}")
+    print(f"Last problem: {lastProblemName}")
+    break
