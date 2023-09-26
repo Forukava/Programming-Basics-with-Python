51ba381b768777e29195eb1bd3bdcb7f99fc2643
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-09-26 19:22:44 +0300

    Update 02. Exam Preparation.py

--- While Loop - Exercise/02. Exam Preparation.py
+++ While Loop - Exercise/02. Exam Preparation.py
@@ -1,24 +1,31 @@
-poorGradeLimit =int(input())
+poorGradeLimit = int(input())
 poorGradeCount = 0
 problemCount = 0
 gradesSum = 0
 lastProblemName = ""
 problemName = ""
 grade = 0
+
 while True:
     problemName = input()
-    if problemName != ("Enough"):
+    if problemName != "Enough":
+        grade = int(input())
+        gradesSum += grade
+        problemCount += 1
+
         if grade <= 4:
             poorGradeCount += 1
-            grade = int(input())
-        if (poorGradeCount >= poorGradeLimit):
+
+        if poorGradeCount >= poorGradeLimit:
             print(f"You need a break, {poorGradeCount} poor grades.")
             break
-        gradesSum += grade
+
         lastProblemName = problemName
-        problemCount += 1
-        continue
-    print(f"Average score: {(gradesSum / problemCount):.2f}")
-    print(f"Number of problems: {problemCount}")
-    print(f"Last problem: {lastProblemName}")
-    break
+    else:
+        if problemCount == 0:
+            print("No problems solved yet.")
+        else:
+            print(f"Average score: {gradesSum / problemCount:.2f}")
+            print(f"Number of problems: {problemCount}")
+            print(f"Last problem: {lastProblemName}")
+        break
