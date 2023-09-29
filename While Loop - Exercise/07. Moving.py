b124b94c2f210e0489155185a58df1d6636c7e56
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-09-29 12:19:47 +0300

    Update 07. Moving.py

--- While Loop - Exercise/07. Moving.py
+++ While Loop - Exercise/07. Moving.py
@@ -1,2 +1,22 @@
 width = int(input())
-lenght = int(input())
+length = int(input())
+heiht = int(input())
+
+area = width * length * heiht
+total = 0
+final = 0
+while True:
+    numbers = input()
+    if numbers != "Done":
+        current = int(numbers)
+        total += current
+        final = area - total
+        if final < 0:
+            break
+    elif numbers == "Done":
+        break
+
+if final < 0:
+    print(f"No more free space! You need {-final} Cubic meters more.")
+else:
+    print(f"{final} Cubic meters left.")
