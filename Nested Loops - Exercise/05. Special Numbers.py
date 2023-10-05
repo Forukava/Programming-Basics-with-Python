cac7d97c0169671ee95c015298ff93b1d032d56b
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-10-05 15:03:06 +0300

    Update 05. Special Numbers.py

--- Nested Loops - Exercise/05. Special Numbers.py
+++ Nested Loops - Exercise/05. Special Numbers.py
@@ -1,4 +1,17 @@
-num = int(input())
+number = int(input())
 
 for num in range(1111, 10000):
     is_special = True
+    num_as_str = str(num)
+    for _, digit in enumerate(num_as_str):
+        current_digit = int(digit)
+
+        if current_digit == 0:
+            is_special = False
+            break
+        if not number % current_digit == 0:
+            is_special = False
+            break
+
+    if is_special:
+        print(f"{num}", end=" ")
