3c5102fc18ab4fe6a75d61264a5cbd89c9f223e0
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2023-10-12 11:49:49 +0300

    -

--- Tests/Tests 10-19/16. class/main.py
+++ Tests/Tests 10-19/16. class/main.py
@@ -1,6 +1,23 @@
-import another_module
-print(another_module.another_variable)
+#import another_module
+#print(another_module.another_variable)
 
-from turtle import Turtle
-dark = Turtle()
-print(dark)
+'''turtle example'''
+#from turtle import Turtle, Screen
+#dark = Turtle()
+#print(dark)
+#dark.shape('turtle')
+#dark.color("red")
+#dark.forward(100)
+#my_screen = Screen()
+#print(my_screen.canvheight)
+#my_screen.exitonclick()
+
+'''Pretty table example'''
+from prettytable import PrettyTable
+table = PrettyTable()
+table.field_names = ["Pokemon Name", "Type"]
+table.add_row(["Picachu", "Electric"])
+table.add_row(["Squrtle", "Water"])
+table.add_row(["Charmander", "Fire"])
+table.align = "l"
+print(table)
